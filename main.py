ff83593a2c95366d4081a94e6de96a066eabef9b
NinjaRacc <80845089+MrBigBoob@users.noreply.github.com>
2022-07-15 23:44:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import time
+
+saved_dicks = open('generated_cocks.txt', 'a', encoding="utf-8")
+
+# remove if you're a skid
+print("""
+  __  __           _        ____          _   _ _       _       _____                
+ |  \/  |         | |      |  _ \        | \ | (_)     (_)     |  __ \               
+ | \  / | __ _  __| | ___  | |_) |_   _  |  \| |_ _ __  _  __ _| |__) |__ _  ___ ___ 
+ | |\/| |/ _` |/ _` |/ _ \ |  _ <| | | | | . ` | | '_ \| |/ _` |  _  // _` |/ __/ __|
+ | |  | | (_| | (_| |  __/ | |_) | |_| | | |\  | | | | | | (_| | | \ \ (_| | (_| (__ 
+ |_|  |_|\__,_|\__,_|\___| |____/ \__, | |_| \_|_|_| |_| |\__,_|_|  \_\__,_|\___\___|
+                                   __/ |              _/ |                           
+                                  |___/              |__/                            
+https://github.com/mrbigboob
+
+""")
+
+time.sleep(5)
+
+i = 0
+while i < 9999999999999999:
+    
+    i += 1
+    time.sleep(0.1)
+    saved_dicks.write("⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⢉⢉⠉⠉⠻⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⣿⣿⣿⠟⠠⡰⣕⣗⣷⣧⣀⣅⠘⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⣿⣿⠃⣠⣳⣟⣿⣿⣷⣿⡿⣜⠄⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⡿⠁⠄⣳⢷⣿⣿⣿⣿⡿⣝⠖⠄⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⠃⠄⢢⡹⣿⢷⣯⢿⢷⡫⣗⠍⢰⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⠃⠄⢢⡹⣿⢷⣯⢿⢷⡫⣗⠍⢰⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⡏⢀⢄⠤⣁⠋⠿⣗⣟⡯⡏⢎⠁⢸⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⠄⢔⢕⣯⣿⣿⡲⡤⡄⡤⠄⡀⢠⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⠇⠠⡳⣯⣿⣿⣾⢵⣫⢎⢎⠆⢀⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⠇⠠⡳⣯⣿⣿⣾⢵⣫⢎⢎⠆⢀⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⠄⢨⣫⣿⣿⡿⣿⣻⢎⡗⡕⡅⢸⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⠄⢜⢾⣾⣿⣿⣟⣗⢯⡪⡳⡀⢸⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⠄⢸⢽⣿⣷⣿⣻⡮⡧⡳⡱⡁⢸⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⡄⢨⣻⣽⣿⣟⣿⣞⣗⡽⡸⡐⢸⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⡇⢀⢗⣿⣿⣿⣿⡿⣞⡵⡣⣊⢸⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⡀⡣⣗⣿⣿⣿⣿⣯⡯⡺⣼⠎⣿⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣧⠐⡵⣻⣟⣯⣿⣷⣟⣝⢞⡿⢹⣿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⡆⢘⡺⣽⢿⣻⣿⣗⡷⣹⢩⢃⢿⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⣷⠄⠪⣯⣟⣿⢯⣿⣻⣜⢎⢆⠜⣿⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⣿⣿⣿⡆⠄⢣⣻⣽⣿⣿⣟⣾⡮⡺⡸⠸⣿⣿⣿⣿\n")
+    saved_dicks.write("⣿⣿⡿⠛⠉⠁⠄⢕⡳⣽⡾⣿⢽⣯⡿⣮⢚⣅⠹⣿⣿⣿\n")
+    saved_dicks.write("⡿⠋⠄⠄⠄⠄⢀⠒⠝⣞⢿⡿⣿⣽⢿⡽⣧⣳⡅⠌⠻⣿\n")
+    saved_dicks.write("⠁⠄⠄⠄⠄⠄⠐⡐⠱⡱⣻⡻⣝⣮⣟⣿⣻⣟⣻⡺⣊\n")
+    saved_dicks.write("\n")
+    time.sleep(0.1)
+    saved_dicks.close
+    print('cocks generated: ', i)
